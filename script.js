8031b3d93e721b5fb980ca102cad7465fa122c32
GGozdziewski1994 <g.gozdziewski1994@gmail.com>
2021-10-14 19:49:16 +0200

    Added project

---
+++ script.js
@@ -0,0 +1,85 @@
+'use strict';
+
+const player0El = document.querySelector(".player-0");
+const player1El = document.querySelector(".player-1");
+const score0El = document.getElementById("score-0");
+const score1El = document.getElementById("score-1");
+const current0El = document.getElementById("current-0");
+const current1El = document.getElementById("current-1");
+
+const diceEl = document.querySelector(".dice");
+const btnNewGame = document.querySelector(".btn-new");
+const btnRollDice = document.querySelector(".btn-roll");
+const btnCatch = document.querySelector(".btn-catch");
+
+let currentScore, scores, activePlayer, winner;
+
+const init = () => {
+    currentScore = 0;
+    scores = [0, 0];
+    activePlayer = 0;
+    winner = false;
+
+    diceEl.classList.add("hidden");
+    score0El.textContent = "0";
+    score1El.textContent = "0";
+
+    player0El.classList.add("player-active");
+    player1El.classList.remove("player-active");
+
+    player0El.classList.remove("player-winner");
+    player1El.classList.remove("player-winner");
+
+    current0El.textContent = "0";
+    current1El.textContent = "0";
+}; init();
+
+const changePlayer = () => {
+    document.getElementById(`current-${activePlayer}`).textContent = 0;
+    currentScore = 0;
+
+    if(winner) return;
+    
+    activePlayer = activePlayer === 0 ? 1 : 0;
+
+    player0El.classList.toggle("player-active");
+    player1El.classList.toggle("player-active");
+}
+
+btnRollDice.addEventListener("click", function () {
+
+    if(winner) return;
+    
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    
+    diceEl.classList.remove("hidden");
+    diceEl.src = `dice-${dice}.png`;
+
+    if(dice !== 1) {
+        currentScore += dice;
+        document.getElementById(`current-${activePlayer}`).textContent = currentScore;
+
+    } else {
+        changePlayer();
+    }
+})
+
+btnCatch.addEventListener("click", function () {
+
+    if(winner) return;
+
+    scores[activePlayer] += currentScore;
+
+    document.getElementById(`score-${activePlayer}`).textContent = scores[activePlayer];  
+    
+    if(scores[activePlayer] >= 100) {
+        document.querySelector(`.player-${activePlayer}`).classList.add("player-winner");
+        document.querySelector(`.player-${activePlayer}`).classList.remove("player-active");
+
+        diceEl.classList.add("hidden");
+        winner = true;
+    } 
+    changePlayer();
+})
+
+btnNewGame.addEventListener("click", init);
